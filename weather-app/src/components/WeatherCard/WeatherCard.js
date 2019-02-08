bdb69f49bef45dbe72d11582f3bf2ca75c859dc8
Graham <gramclee@gmail.com>
2019-02-07 16:37:11 -0800

    Added more styling to weather cards

--- weather-app/src/components/WeatherCard/WeatherCard.js
+++ weather-app/src/components/WeatherCard/WeatherCard.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react'
+import Proptypes from 'prop-types'
 import './style.css'
 import '../../common/css/weather-icons.css'
 
 class WeatherCard extends Component {
     render(){
+        const { dayName, highTemp, lowTemp } = this.props
         return(
         <div className='weatherCard'>
-            <i className="wi wi-day-sunny"></i>
+            <div className="dayName">{dayName}</div>
+            <div className="iconContainer">
+                <i className="wi wi-day-sunny"></i>
+            </div>
+            <div className="temperature">
+                <div>{highTemp}&#176;</div>
+                <div>{lowTemp}&#176;</div>
+            </div>
         </div>
         )
     }
+}
+
+WeatherCard.Proptypes = {
+    dayName: Proptypes.string,
+    highTemp: Proptypes.number,
+    lowTemp: Proptypes.number
 }
 
 export default WeatherCard
