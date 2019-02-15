8cc336643fd54917e931837bfd708afacdda5046
Graham <gramclee@gmail.com>
2019-02-14 18:35:11 -0800

    Integrated API call

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,22 +1,63 @@
 import React, { Component } from 'react';
 import './App.css';
+import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import WeatherCard  from './components/WeatherCard/WeatherCard.js';
+import InputGroup from 'react-bootstrap/InputGroup'
+
+const FormControl = Form.Control
 
 class App extends Component {
-  // componentDidMount(){
-  //   fetch('http://api.openweathermap.org/data/2.5/forecast?q=vancouver&APPID=01cfc6c2e6ed975cd2864678e71e55e2').then((res)=>{
-  //     console.log('res:', res.json())
-  //   })
-  // }
+  constructor(props){
+    super(props)
+    this.state = {
+      cityInput: null,
+      weatherData: null,
+    }
+  }
+  generateMessage = () => {
+    if (this.state.weatherData !== null) {
+      return this.state.weatherData.message
+    } else {
+      return ''
+    }
+  }
+  handleSubmit = (e) => {
+    e.preventDefault()
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.cityInput}&APPID=01cfc6c2e6ed975cd2864678e71e55e2`).then((res)=>{
+       return res.json()
+    }).then((res) => {
+      this.setState({weatherData: res})
+    })
+  }
   render() {
     return (
       <div className="App">
       <header className="header">Weather App</header>
         <div style={{marginTop: '0px', marginBottom: '10px'}}>Application that gives the current weather forecast of a city</div>
-        <Button variant="outline-primary">Primary</Button>
+        <Form style={{marginBottom: '40px'}} onSubmit={(e) => this.handleSubmit(e)}>
+          <InputGroup className="mb-3">
+            <FormControl
+              id='cityName'
+              placeholder="City name (ex. Vancouver,CA)"
+              aria-label="city name"
+              onChange={(e) => this.setState({cityInput: e.target.value})}
+            />
+            <InputGroup.Append>
+              <Button type='submit'>Submit</Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </Form>
         <div className="weatherCardContainer">
-          <WeatherCard dayName={'Mon'} highTemp={99} lowTemp={10}/>
+        {this.state.weatherData !== null && this.state.weatherData.cod === 200 ? (
+          <WeatherCard 
+            label={`${this.state.weatherData.name}, ${this.state.weatherData.sys.country}`} 
+            highTemp={this.state.weatherData.main.temp_max} 
+            description={this.state.weatherData.weather[0].description}
+            lowTemp={this.state.weatherData.main.temp_min} 
+            weatherCode={this.state.weatherData.weather[0].id}
+          />
+        ) : this.generateMessage()}
         </div>
       </div>
     );
--- weather-app/src/components/WeatherCard/WeatherCard.js
+++ weather-app/src/components/WeatherCard/WeatherCard.js
@@ -7,17 +7,138 @@ import '../../common/css/weather-icons-wind.css'
 import '../../common/css/weather-icons-wind.min.css'
 
 class WeatherCard extends Component {
+
+    kelvinToCelsius = (kelvins) => {
+        return (kelvins - 273.15).toFixed(1)
+    }
+
+    getIconName = (weatherCode) => {
+        switch(weatherCode){
+            case 201:
+                return 'wi-storm-showers'
+            case 202:
+                return 'wi-storm-showers'
+            case 210:
+                return 'wi-storm-showers'
+            case 211:
+                return 'wi-thunderstorm'
+            case 212:
+                return 'wi-thunderstorm'
+            case 221:
+                return 'wi-thunderstorm'
+            case 230:
+                return 'wi-thunderstorm'
+            case 231:
+                return 'wi-thunderstorm'
+            case 232:
+                return 'wi-thunderstorm'
+            case 300:
+                return 'wi-sprinkle'
+            case 301:
+                return 'wi-sprinkle'
+            case 302:
+                return 'wi-sprinkle'
+            case 310:
+                return 'wi-sprinkle'
+            case 311:
+                return 'wi-sprinkle'
+            case 312:
+                return 'wi-sprinkle'
+            case 313:
+                return 'wi-sprinkle'
+            case 314:
+                return 'wi-sprinkle'
+            case 321:
+                return 'wi-sprinkle'
+            case 500:
+                return 'wi-rain'
+            case 501:
+                return 'wi-rain'
+            case 502:
+                return 'wi-rain'
+            case 503:
+                return 'wi-rain'
+            case 504:
+                return 'wi-rain'
+            case 511:
+                return 'wi-rain'
+            case 520:
+                return 'wi-rain'
+            case 521:
+                return 'wi-rain'
+            case 522:
+                return 'wi-rain'
+            case 531:
+                return 'wi-rain'
+            case 600:
+                return 'wi-snow'
+            case 601:
+                return 'wi-snow'
+            case 602:
+                return 'wi-snow'
+            case 611:
+                return 'wi-snow'
+            case 612:
+                return 'wi-snow'
+            case 615:
+                return 'wi-snow'
+            case 616:
+                return 'wi-snow'
+            case 620:
+                return 'wi-snow'
+            case 621:
+                return 'wi-snow'
+            case 622:
+                return 'wi-snow'
+            case 701:
+                return 'wi-fog'
+            case 711:
+                return 'wi-smoke'
+            case 721:
+                return 'wi-day-haze'
+            case 731:
+                return 'wi-dust'
+            case 741:
+                return 'wi-fog'
+            case 751:
+                return 'wi-sandstorm'
+            case 761:
+                return 'wi-dust'
+            case 762:
+                return 'wi-dust' 
+            case 771:
+                return 'wi-windy'
+            case 781:
+                return 'wi-tornado' 
+            case 800:
+                return 'wi-day-sunny'
+            case 801:
+                return 'wi-cloud'
+            case 802:
+                return 'wi-cloudy' 
+            case 803:
+                return 'wi-cloudy'
+            case 804:
+                return 'wi-cloudy'    
+            default:
+                return ''
+        }
+    }
+
     render(){
-        const { dayName, highTemp, lowTemp } = this.props
+        const { label, highTemp, lowTemp, weatherCode, description } = this.props
         return(
         <div className='weatherCard'>
-            <div className="dayName">{dayName}</div>
+            <div className="dayName">{label}</div>
             <div className="iconContainer">
-                <i className="wi wi-day-sunny"></i>
+                <i className={`wi ${this.getIconName(weatherCode)}`}></i>
+            </div>
+            <div className="description">
+                {description}
             </div>
             <div className="temperature">
-                <div>{highTemp}&#176;</div>
-                <div>{lowTemp}&#176;</div>
+                <div>{this.kelvinToCelsius(highTemp)}&#176;C</div>
+                <div>{this.kelvinToCelsius(lowTemp)}&#176;C</div>
             </div>
         </div>
         )
@@ -25,7 +146,9 @@ class WeatherCard extends Component {
 }
 
 WeatherCard.Proptypes = {
-    dayName: Proptypes.string,
+    weatherCode: Proptypes.number,
+    label: Proptypes.string,
+    description: Proptypes.string,
     highTemp: Proptypes.number,
     lowTemp: Proptypes.number
 }
