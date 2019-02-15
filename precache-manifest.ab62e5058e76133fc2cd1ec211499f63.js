2b7271c55d970cad0eeccf98026bcd7e89ae99f3
Graham <gramclee@gmail.com>
2019-02-14 20:53:36 -0800

    Updates

---
+++ precache-manifest.ab62e5058e76133fc2cd1ec211499f63.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/WebWeatherApp/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/WebWeatherApp/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/WebWeatherApp/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/WebWeatherApp/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/WebWeatherApp/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "7ccca8ca87a32f9aad1a",
+    "url": "/WebWeatherApp/static/js/runtime~main.7ccca8ca.js"
+  },
+  {
+    "revision": "160cb7aafa08111082a2",
+    "url": "/WebWeatherApp/static/js/main.160cb7aa.chunk.js"
+  },
+  {
+    "revision": "fe3c875a6e3d06c74946",
+    "url": "/WebWeatherApp/static/js/1.fe3c875a.chunk.js"
+  },
+  {
+    "revision": "160cb7aafa08111082a2",
+    "url": "/WebWeatherApp/static/css/main.13f3e732.chunk.css"
+  },
+  {
+    "revision": "6e7cae19911bbd6bbe74dc322547c92d",
+    "url": "/WebWeatherApp/index.html"
+  }
+];
